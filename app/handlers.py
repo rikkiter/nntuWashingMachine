0be5efad6f2b28b00a3b8ab8085d887196f3cd5d
rikkiter <rika5637@mail.ru>
2025-08-07 17:49:58 +0300

    nothing interesting

--- app/handlers.py
+++ app/handlers.py
@@ -17,3 +17,13 @@ async def s(message: Message) -> None:
 async def m(massage: Message) -> None:
     print(massage.text)
     await massage.answer(text="test", reply_markup=kb.machine_manager)
+
+
+@router.message(F.text == EXIT_RU)
+async def exit_to_start(message: Message) -> None:
+    await s(message)
+
+
+@router.message(Command("about"))
+async def about(message: Message) -> None:
+    await message.answer("text")
